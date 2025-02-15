6ebb8ed99b64550f31a49a671c4e5291707bd1b3
FrancescoDiBenedetto <francescomdibe@gmail.com>
2025-02-15 18:27:52 -0300

    criando o primeiro produto

--- Aula 02 - 14-02-2025 - R/Aula02/src/main/java/br/com/fiap/Main.java
+++ Aula 02 - 14-02-2025 - R/Aula02/src/main/java/br/com/fiap/Main.java
@@ -5,8 +5,10 @@ import br.com.fiap.model.Produto;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        Produto produto01 = new Produto("Monster", "Monster", 473, 473);
+        Produto produto01 = new Produto("Energético", "Monster", 10.0, 473);
         System.out.println(produto01.getMarca());
+        System.out.println(produto01.getNome());
+        System.out.println(produto01.getPreco());
 
     }
 }
